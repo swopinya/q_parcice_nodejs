45552b7b8547139f141fe52702c3ecc45490dba0
swopinya <shimon@opinya.co.il>
2015-11-02 11:46:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by root on 02/11/15.
+ */
+
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+//var path = require('path');
+//var bodyParser = require('body-parser');
+
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+
+// use middleware
+//app.use(express.static(path.join(__dirname, 'views')));
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+// define routes
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
+app.use(require('./q_practice'));
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(){
+    console.log("app http ready on port "+port);
+});
---
+++ q_practice.js
@@ -0,0 +1,10 @@
+/**
+ * Created by root on 02/11/15.
+ */
+
+var express = require('express');
+var q_practice = express.Router();
+
+
+
+module.exports = q_practice;
